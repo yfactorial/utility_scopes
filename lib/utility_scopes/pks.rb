e3b6f6cff25318973df1d0988b65df53a38e014d
Ryan Daigle <ryan.daigle@gmail.com>
2009-12-11 17:33:26 -0500

    Fix pks scope

--- lib/utility_scopes/pks.rb
+++ lib/utility_scopes/pks.rb
@@ -3,13 +3,18 @@ module UtilityScopes
     
     def self.included(within)
       
-      within.class_eval do
-        named_scope :only_pks, :select => "`#{table_name}`.#{primary_key}"        
+      within.class_eval do        
         extend ClassMethods
       end
     end
     
     module ClassMethods
+
+      # Can't use named_scope b/c don't have access to table_name etc... yet
+      def only_pks
+        scoped(:select => "`#{table_name}`.#{primary_key}")
+      end
+
       def pks
         only_pks.collect(&:"#{primary_key}")
       end
